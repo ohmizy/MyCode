26caa68a3ac18ec319c588afcbdf57c1adc708aa
ohmizy <omarjabr49@yahoo.com>
2022-07-28 08:59:41 -0700

    Code from 7/28/20

---
+++ WebFundamentalsExam/petShelter.js
@@ -0,0 +1,15 @@
+function hide(element) {
+  element.remove ()
+}
+
+function hide(element) {
+  element.remove ()
+}
+
+function picked(element) {
+  alert('You picked' + element.value);
+}
+
+function like (element){
+  element.innerText++;
+  }
